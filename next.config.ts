f7cd76fc781d9d40a144ec3a0527803bf71b361a
Kristian <123009027+xKold@users.noreply.github.com>
2025-07-03 08:17:20 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import path from 'path';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias['@lib'] = path.resolve(__dirname, 'lib');
+    return config;
+  },
 };
 
 export default nextConfig;
