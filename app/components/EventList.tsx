247357b3b6cf91bf823244758ac675942f00e427
Kristian <123009027+xKold@users.noreply.github.com>
2025-07-03 21:51:58 -0400

    test

--- app/components/EventList.tsx
+++ app/components/EventList.tsx
@@ -14,13 +14,14 @@ const events: Event[] = [
     id: 'f56331e8-131a-4c50-b7db-eec8b010ff98',
     name: 'Off Season Shenanigans S8',
     region: 'North America',
-    startDate: '2025-07-01',
+    startDate: '2025-06-01',
+    endDate: '2025-07-01',
   },
   {
     id: '81e36970-81ec-4e53-b2af-c0a1c0b52938',
     name: 'Off Season Shenanigans S8 Playoffs',
     region: 'North America',
-    startDate: '2025-07-XX', // put actual date if you want
+    startDate: '2025-07-1', // put actual date if you want
   },
 ];
 
